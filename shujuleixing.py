a2e9bd9bbce929ea12dcb19f44b45aa0dc2cf56c
David <david@gmail.com>
2019-04-25 09:51:24 +0800

    数据类型demo

---
+++ shujuleixing.py
@@ -0,0 +1,22 @@
+# 数据类型转换
+
+a = '520'
+b = int(a)
+print(b)
+# b = 520
+
+c = 5.99
+d = int(c)
+print(d)
+# d = 5  直接舍小数
+
+a = '520'
+b = float(a)
+print(b)
+# b = 520.0
+
+a = 5.99
+b = str(a)
+print(b)
+# b = '5.99'
+
